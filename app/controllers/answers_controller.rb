6783f559c0224ed329389758efdb934b081c73b9
Tammam Kbeili <tammam.kbeili@gmail.com>
2015-11-13 14:02:38 -0800

    Add a mailer to notify question owner when they get answers

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -13,6 +13,7 @@ class AnswersController < ApplicationController
     @answer.question = @q
     # byebug
     if @answer.save
+      AnswersMailer.notify_question_owner(@answer).deliver_now
       # redirect_to(question_path(q), {notice: "Answer created successfully!"})
       redirect_to question_path(@q), notice: "Answer created successfully!"
     else
